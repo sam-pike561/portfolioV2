4c2430aec511124b78dde494bf1932cfe2d8ae66
Samuel Pike <sampike1994@gmail.com>
2021-10-29 13:50:02 -0400

    First commit

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,24 @@
 import "./intro.scss"
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
-export default function intro() {
+export default function Intro() {
     return (
-        <div className="intro">
-            
+        <div className="intro" id="intro">
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/circlePortrait.png" alt="" />
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hi There, I'm</h2>
+                    <h1>Samuel Pike</h1>
+                    <h3>Freelance <span>Designer</span></h3>
+                </div>
+                <a href="#portfolio">
+                   <KeyboardArrowDownIcon />
+                </a>
+            </div>
         </div>
     )
 }
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,31 @@
+import MailIcon from '@mui/icons-material/Mail';
+import PrecisionManufacturingIcon from '@mui/icons-material/PrecisionManufacturing';
+
 import "./topbar.scss";
 
-export default function Topbar() {
+export default function Topbar({ menuOpen, setMenuOpen }) {
     return (
-        <div className="topbar">
-            
+        <div className={"topbar " + (menuOpen && "active")}>
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#intro" className="logo">PIKE  | PORTFOLIO</a>
+                    <div className="itemContainer">
+                        <PrecisionManufacturingIcon className="icon"/>
+                        <span>+1 (561) 427 3250</span>
+                    </div>
+                    <div className="itemContainer">
+                        <MailIcon className="icon"/>
+                        <span>samuelp1dev@gmail.com</span>
+                    </div>
+                </div>
+                <div className="right">
+                    <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
+                        <span className="line1"></span>
+                        <span className="line2"></span>
+                        <span className="line3"></span>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
