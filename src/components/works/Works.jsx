cc28bc28e5a6747b17ec68f94f828df3415a8a30
Samuel Pike <sampike1994@gmail.com>
2021-11-05 17:19:10 -0400

    added dummy data to works.jsx

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,30 +1,44 @@
 import MobileFriendlyIcon from '@mui/icons-material/MobileFriendly';
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import "./works.scss"
 
 export default function Works() {
     
     const data = [
+            {
+        id: 1,
+        title: "E-Commerce App",
+        img: "./assets/3.png"
+    },
         {
-            id: "featured",
-            title: "Featured App",
-        },
-        {
-            id: "web",
-            title: "Web App",
-        },
-        {
-            id: "mobile",
-            title: "Mobile App",
-        },
-        {
-            id: "design",
-            title: "Design App",
-        },
-        {
-            id: "content",
-            title: "Content",
-        }
+        id: 2,
+        title: "SurvStat",
+        img: "./assets/2.png"
+    },
+            {
+        id: 3,
+        title: "Wiki App",
+        img: "./assets/1.png"
+    },
+                {
+        id: 4,
+        title: "WebRTC App",
+        img: "./1.png"
+    },
+                    {
+        id: 5,
+        title: "Amazonia",
+        img: "./1.png"
+    },
+                     {
+        id: 6,
+        title: "Amazonia",
+        img: "./1.png"
+    },
+                      {
+        id: 7,
+        title: "Amazonia",
+        img: "./1.png"
+    },
     ];
     return (
         <div className="works" id="works">
@@ -47,8 +61,9 @@ export default function Works() {
                     </div>
                 </div>
             </div>
-            <ArrowForwardIosIcon className="arrow left" />
-            <ArrowForwardIosIcon className="arrow right" />
+            <img src="./assets/rightArrow.png" className="arrow right" alt="" />
+            <img src="./assets/rightArrow.png" className="arrow left" alt=""  />
+
 
            
         </div>
